0475e146843e37e3ba98759a3d12fa36a72b72a2
ui-richard <richard.tsai@ui.com>
2021-04-14 14:14:49 +0800

    create folder, CMakeList.txt for formant shift

---
+++ samples/formant_shifter/src/main.cpp
@@ -0,0 +1,92 @@
+#include "CLI/CLI.hpp"
+#include <sndfile.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char **argv) {
+    std::string inputFilePath, pitchFilePath, outputFilePath;
+    short *raw_data, *ps_data, *out_data;
+    int frame_size = 1024;
+
+    CLI::App app{"Formant Shifter"};
+
+    app.add_option("-i,--inFile", inputFilePath, "specify the original input file")
+        ->required()
+        ->check(CLI::ExistingFile);
+    app.add_option("-p,--pitchFile", pitchFilePath, "specify the pitch shift file")
+        ->required()
+        ->check(CLI::ExistingFile);
+    app.add_option("-o,--outFile", outputFilePath, "specify an output file")->required();
+    app.add_option("--frameSize", frame_size, "frame size in samples")->check(CLI::Number);
+
+    try {
+        app.parse(argc, argv);
+    } catch (const CLI::ParseError &e) { return app.exit(e); }
+
+    SNDFILE *infile, *psfile, *outfile;
+
+    SF_INFO in_sfinfo, ps_sfinfo;
+    memset(&in_sfinfo, 0, sizeof(SF_INFO));
+    memset(&ps_sfinfo, 0, sizeof(SF_INFO));
+
+    if (!(infile = sf_open(inputFilePath.c_str(), SFM_READ, &in_sfinfo))) {
+        printf("Not able to open input file %s.\n", inputFilePath.c_str());
+        puts(sf_strerror(NULL));
+        return 1;
+    };
+
+    if (!(psfile = sf_open(pitchFilePath.c_str(), SFM_READ, &ps_sfinfo))) {
+        printf("Not able to open input file %s.\n", pitchFilePath.c_str());
+        puts(sf_strerror(NULL));
+        return 1;
+    };
+
+	if ((ps_sfinfo.samplerate != in_sfinfo.samplerate) 
+			|| ps_sfinfo.channels != 1 
+			|| in_sfinfo.channels != 1) {
+        printf("Sample rate or channel number doesn't match");
+		return 1;
+	}
+
+    if (!(outfile = sf_open(outputFilePath.c_str(), SFM_WRITE, &in_sfinfo))) {
+        printf("Not able to open output file %s.\n", outputFilePath.c_str());
+        puts(sf_strerror(NULL));
+        return 1;
+    };
+
+    int sample_rate = in_sfinfo.samplerate;
+    printf("sample rate: %d\n", sample_rate);
+
+    raw_data = (short *)new short[frame_size];
+    ps_data = (short *)new short[frame_size];
+    out_data = (short *)new short[frame_size];
+
+    clock_t tick = clock();
+
+    int count = 0;
+
+    while ((frame_size != sf_read_short(infile, raw_data, frame_size))
+				|| (frame_size != sf_read_short(psfile, ps_data, frame_size))) {
+        /* run something here */
+        /* run something here */
+        /* run something here */
+        sf_write_short(outfile, raw_data, frame_size);
+        count++;
+    }
+
+    tick = clock() - tick;
+    printf("total tick: %ld, times: %f\n", tick, ((float)tick / CLOCKS_PER_SEC));
+
+    /* Close input and output files. */
+    sf_close(infile);
+    sf_close(psfile);
+    sf_close(outfile);
+
+    delete[] raw_data;
+    delete[] ps_data;
+    delete[] out_data;
+
+    return 0;
+}
