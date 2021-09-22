e76a840abbe2f81d3187347291dfa8589447bda0
ui-richard <richard.tsai@ui.com>
2021-09-22 14:27:11 +0800

    update(click_removal): update test cases for biquad

--- modules/click_removal/test/test_biquad.cpp
+++ modules/click_removal/test/test_biquad.cpp
@@ -39,8 +39,8 @@ TEST(SosFilter, Process) {
     float *inbuf = new float[frameSize];
     float *outbuf = new float[frameSize];
     float ans[16] = {0.,          0.32483446,  0.09298379, -0.2415354, -0.36018799, -0.24880247,
-                   -0.04897011, 0.11620218,  0.20197575, 0.21500525, 0.174264,    0.09675092,
-                   0.00131773,  -0.08798588, -0.1468685, -0.15983416};
+                     -0.04897011, 0.11620218,  0.20197575, 0.21500525, 0.174264,    0.09675092,
+                     0.00131773,  -0.08798588, -0.1468685, -0.15983416};
 
     sosfilt.reset(ba, 2);
     for (int i = 0; i < frameSize; i++) { inbuf[i] = i; }
@@ -48,6 +48,66 @@ TEST(SosFilter, Process) {
 
     for (int i = 0; i < frameSize; i++) {
         // printf("%d\t%f\t%f\n", i, outbuf[i], ans[i]);
+        ASSERT_LT(fabs(outbuf[i] - ans[i]), 1e-3);
+    }
+
+    delete[] inbuf;
+    delete[] outbuf;
+}
+
+TEST(SosFilter, HPF1kHz) {
+    SosFilter sosfilt;
+
+    // hpf 1kHz for fs=48kHz
+    // from scipy import signal
+    // sos_hpf = signal.cheby1(4, 3, 1000/24000, 'high', output='sos')
+    const float hpf_1kHz[2][5] = {{0.60030784, -1.20061568, 0.60030784, -1.68713702, 0.76275989},
+                                  {1., -2., 1., -1.95698292, 0.97569048}};
+
+    int frameSize = 16;
+    float *inbuf = new float[frameSize];
+    float *outbuf = new float[frameSize];
+    float ans[16] = {0.,          0.60030784,  0.98697809,  1.1713336,   1.178229,    1.04111067,
+                     0.79757528,  0.48564276,  0.14085483,  -0.20577818, -0.52902836, -0.80999444,
+                     -1.03606497, -1.20043981, -1.30134908, -1.34109836};
+
+    for (int i = 0; i < 16; i++) { inbuf[i] = (float)i; }
+    sosfilt.reset(hpf_1kHz, 2);
+    sosfilt.process(inbuf, outbuf, 16);
+
+    for (int i = 0; i < frameSize; i++) {
+        printf("%d\t%f\t%f\n", i, outbuf[i], ans[i]);
+        ASSERT_LT(fabs(outbuf[i] - ans[i]), 1e-3);
+    }
+
+    delete[] inbuf;
+    delete[] outbuf;
+}
+
+TEST(SosFilter, LPF1kHz) {
+    SosFilter sosfilt;
+
+    // lpf 1kHz for fs=48kHz
+    // from scipy import signal
+    // sos_lpf = signal.cheby1(4, 3, 1000/24000, 'low', output='sos')
+    const float lpf_1kHz[2][5] = {
+        {2.21649547e-06, 4.43299093e-06, 2.21649547e-06, -1.94427324e+00, 9.47549838e-01},
+        {1.00000000e+00, 2.00000000e+00, 1.00000000e+00, -1.96271306e+00, 9.78001501e-01}};
+
+    int frameSize = 16;
+    float *inbuf = new float[frameSize];
+    float *outbuf = new float[frameSize];
+    float ans[16] = {0.00000000e+00, 2.21649547e-06, 2.19587902e-05, 1.10746881e-04,
+                     3.85872611e-04, 1.05866521e-03, 2.45871894e-03, 5.05380838e-03,
+                     9.46529467e-03, 1.64789054e-02, 2.70508494e-02, 4.23093039e-02,
+                     6.35513889e-02, 9.22358102e-02, 1.29971422e-01, 1.78502021e-01};
+
+    for (int i = 0; i < 16; i++) { inbuf[i] = (float)i; }
+    sosfilt.reset(lpf_1kHz, 2);
+    sosfilt.process(inbuf, outbuf, 16);
+
+    for (int i = 0; i < frameSize; i++) {
+        printf("%d\t%f\t%f\n", i, outbuf[i], ans[i]);
         ASSERT_LT(fabs(outbuf[i] - ans[i]), 1e-3);
     }
 
