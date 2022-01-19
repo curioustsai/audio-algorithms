536be71f0d90ef6eb76fb900a601fb9b7395488e
ui-richard <richard.tsai@ui.com>
2022-01-19 17:40:46 +0800

    update(ui_athena): apply ring buffer to simulate different frame size between system and algorithms

--- samples/ui_athena/src/main.cpp
+++ samples/ui_athena/src/main.cpp
@@ -1,12 +1,15 @@
 #include "CLI/CLI.hpp"
+#include "CLI/Validators.hpp"
+#include "nlohmann/json.hpp"
+
 #include <sndfile.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-#include "CLI/Validators.hpp"
-#include "nlohmann/json.hpp"
+#include "ring_buffer.h"
+#include "ubnt_logger/ubnt_logger.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -27,7 +30,7 @@ int main(int argc, char **argv) {
     app.add_option("-i,--inFile", inputFilePath, "specify an input file")
         ->required()
         ->check(CLI::ExistingFile);
-    app.add_option("-r,--refFile", refFilePath, "specify an ref file");
+    app.add_option("-r,--refFile", refFilePath, "specify an ref file")->check(CLI::ExistingFile);
     app.add_option("-o,--outFile", outputFilePath, "specify an output file")->required();
     app.add_option("-c,--config", jsonConfigPath, "specify an json config file")
         ->required()
@@ -37,48 +40,30 @@ int main(int argc, char **argv) {
         app.parse(argc, argv);
     } catch (const CLI::ParseError &e) { return app.exit(e); }
 
-    SF_INFO info;
-    SNDFILE *inwav = sf_open(inputFilePath.c_str(), SFM_READ, &info);
-    if (NULL == inwav) {
-        fprintf(stderr, "open %s failed\n", inputFilePath.c_str());
-        return -1;
-    }
+    SF_INFO info, rinfo, oinfo;
+    SNDFILE *inwav, *refwav, *outwav;
 
-    SF_INFO rinfo;
-    SNDFILE *refwav;
+    ASSERT_LOG_RET(inwav = sf_open(inputFilePath.c_str(), SFM_READ, &info), -1, "open %s failed",
+                   inputFilePath.c_str());
+
     if (!refFilePath.empty()) {
-        refwav = sf_open(refFilePath.c_str(), SFM_READ, &rinfo);
-        if (NULL == refwav) {
-            fprintf(stderr, "open %s failed\n", refFilePath.c_str());
-            sf_close(inwav);
-            return -1;
-        }
+        ASSERT_LOG_ACT(refwav = sf_open(refFilePath.c_str(), SFM_READ, &rinfo), sf_close(inwav), -1,
+                       "open %s failed", refFilePath.c_str());
     }
 
-    SF_INFO onfo;
-    onfo.channels = 1;
-    onfo.samplerate = info.samplerate;
-    onfo.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
-    onfo.sections = 0;
-    onfo.seekable = 1;
-    SNDFILE *outwav = sf_open(outputFilePath.c_str(), SFM_WRITE, &onfo);
-    if (NULL == outwav) {
-        fprintf(stderr, "open %s failed\n", outputFilePath.c_str());
-        sf_close(inwav);
-        sf_close(refwav);
-        return -2;
-    }
+    oinfo.channels = 1;
+    oinfo.samplerate = info.samplerate;
+    oinfo.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
+    oinfo.sections = 0;
+    oinfo.seekable = 1;
+
+    ASSERT_LOG_ACT(outwav = sf_open(outputFilePath.c_str(), SFM_WRITE, &oinfo),
+                   sf_close(inwav) && sf_close(refwav), -1, "open %s failed",
+                   outputFilePath.c_str());
 
     // read json config file
     std::ifstream ifs(jsonConfigPath);
     nlohmann::json jf = nlohmann::json::parse(ifs);
-    std::string js_str = jf.dump();
-    // std::cout << "json string: " << js_str << std::endl;
-
-    // // iterate all items
-    // for (auto & el : jf.items()) {
-    //     std::cout << el.key() << " : " << el.value() << std::endl;
-    // }
 
     objSSP_Param param;
     param.AEC_KEY = jf["AEC_KEY"];
@@ -91,76 +76,98 @@ int main(int argc, char **argv) {
     param.ref_num = jf["ref_num"];
     param.loc_phi = jf["loc_phi"];
 
-    param.aec_param.dt_thr_factor = jf["aec"]["dt_thr_factor"];
-    param.aec_param.dt_min_thr = jf["aec"]["dt_min_thr"];
+    if (param.AEC_KEY == 1) {
+        param.aec_param.dt_thr_factor = jf["aec"]["dt_thr_factor"];
+        param.aec_param.dt_min_thr = jf["aec"]["dt_min_thr"];
 
-    param.aec_param.res1_echo_noise_factor = jf["aec"]["res1_echo_noise_factor"];
-    param.aec_param.res2_echo_noise_factor = jf["aec"]["res2_echo_noise_factor"];
-    param.aec_param.res1_echo_suppress_default = jf["aec"]["res1_echo_suppress_default"];
-    param.aec_param.res2_st_echo_suppress_default = jf["aec"]["res2_st_echo_suppress_default"];
-    param.aec_param.res2_dt_echo_suppress_default = jf["aec"]["res2_dt_echo_suppress_default"];
-    param.aec_param.res1_echo_suppress_active_default = jf["aec"]["res1_echo_suppress_active_default"];
-    param.aec_param.res2_st_echo_suppress_active_default = jf["aec"]["res2_st_echo_suppress_active_default"];
-    param.aec_param.res2_dt_echo_suppress_active_default = jf["aec"]["res2_dt_echo_suppress_active_default"];
-    param.aec_param.res1_suppress_factor = jf["aec"]["res1_suppress_factor"];
-    param.aec_param.res2_st_suppress_factor = jf["aec"]["res2_st_suppress_factor"];
-    param.aec_param.res2_dt_suppress_factor = jf["aec"]["res2_dt_suppress_factor"];
+        param.aec_param.res1_echo_noise_factor = jf["aec"]["res1_echo_noise_factor"];
+        param.aec_param.res2_echo_noise_factor = jf["aec"]["res2_echo_noise_factor"];
+        param.aec_param.res1_echo_suppress_default = jf["aec"]["res1_echo_suppress_default"];
+        param.aec_param.res2_st_echo_suppress_default = jf["aec"]["res2_st_echo_suppress_default"];
+        param.aec_param.res2_dt_echo_suppress_default = jf["aec"]["res2_dt_echo_suppress_default"];
+        param.aec_param.res1_echo_suppress_active_default =
+            jf["aec"]["res1_echo_suppress_active_default"];
+        param.aec_param.res2_st_echo_suppress_active_default =
+            jf["aec"]["res2_st_echo_suppress_active_default"];
+        param.aec_param.res2_dt_echo_suppress_active_default =
+            jf["aec"]["res2_dt_echo_suppress_active_default"];
+        param.aec_param.res1_suppress_factor = jf["aec"]["res1_suppress_factor"];
+        param.aec_param.res2_st_suppress_factor = jf["aec"]["res2_st_suppress_factor"];
+        param.aec_param.res2_dt_suppress_factor = jf["aec"]["res2_dt_suppress_factor"];
+    }
 
     memset(param.mic_coord, 0, sizeof(param.mic_coord));
 
-    void *hssp = dios_ssp_init_api(&param);
-    if (NULL == hssp) {
-        fprintf(stderr, "dios_ssp_init_api failed\n");
-        return -4;
-    }
+    int ret;
+    void *hssp;
 
-    int ret = dios_ssp_reset_api(hssp, &param);
-    if (ret) {
-        fprintf(stderr, "dios_ssp_reset_api failed, return %d\n", ret);
-        return -5;
-    }
+    ASSERT_LOG_RET(hssp = dios_ssp_init_api(&param), -4, "dios_ssp_init_api failed");
+    ASSERT_LOG_RET((ret = dios_ssp_reset_api(hssp, &param)) == 0, -5,
+                   "dios_ssp_reset_api failed, return %d", ret);
 
-    int framelen = 128;
+    // Note: athena-signal only take 128 samples as the frame size, but we apply queue for practical use
+    // Simulation frameSize 1024 on our camera
+    int frameLen = 1024;
+    int subframeLen = 128;
+    int bufNum = frameLen / subframeLen * 8;
+
+    ubnt::RingBuffer<short> inputRingBuf(subframeLen, bufNum);
+    ubnt::RingBuffer<short> refRingBuf(subframeLen, bufNum);
+    ubnt::RingBuffer<short> outputRingBuf(subframeLen, bufNum);
+
+    short micbuf[frameLen];
+    short refbuf[frameLen];
+    short sspbuf[frameLen];
+    short micbuf_sub[subframeLen];
+    short refbuf_sub[subframeLen];
+    short sspbuf_sub[subframeLen];
+
     while (1) {
-        short micbuf[framelen];
-        short refbuf[framelen];
-        short sspbuf[framelen];
-
-        int readsize = sf_readf_short(inwav, micbuf, framelen);
-        if (readsize != framelen) {
+        if (sf_readf_short(inwav, micbuf, frameLen) != frameLen) {
             fprintf(stderr, "processing all data, exiting...\n");
             break;
         }
+        ASSERT_LOG(LOG_INFO, inputRingBuf.putFrame(micbuf, frameLen), "inputRingBuf Full");
 
         if (!refFilePath.empty() && param.AEC_KEY) {
-            readsize = sf_readf_short(refwav, refbuf, framelen);
-            if (readsize != framelen) {
+            if (sf_readf_short(refwav, refbuf, frameLen) != frameLen) {
                 fprintf(stderr, "processing all data, exiting...\n");
                 break;
             }
+            ASSERT_LOG(LOG_INFO, refRingBuf.putFrame(refbuf, frameLen), "refRingBuf Full");
         }
 
-        ret = dios_ssp_process_api(hssp, micbuf, refbuf, sspbuf, &param);
-        if (ret) {
-            fprintf(stderr, "dios_ssp_process_api failed, return %d\n", ret);
-            break;
+        while (inputRingBuf.getInUseSamples() > subframeLen) {
+
+            ASSERT_LOG(LOG_INFO, subframeLen == inputRingBuf.getFrame(micbuf_sub, subframeLen),
+                       "Failed to get subframe from inputRingBuf");
+
+            if (!refFilePath.empty() && param.AEC_KEY) {
+                ASSERT_LOG(LOG_INFO, subframeLen == refRingBuf.getFrame(refbuf_sub, subframeLen),
+                           "Failed to get subframe from refRingBuf");
+            }
+
+            ret = dios_ssp_process_api(hssp, micbuf_sub, refbuf_sub, sspbuf_sub, &param);
+            ASSERT_LOG(LOG_ERR, (ret == 0), "dios_ssp_process_api failed, return %d", ret);
+
+            ASSERT_LOG(LOG_INFO, outputRingBuf.putFrame(sspbuf_sub, subframeLen),
+                       "outputRingBuf Full");
         }
 
-        sf_writef_short(outwav, sspbuf, framelen);
+        while (outputRingBuf.getInUseSamples() > frameLen) {
+            ASSERT_LOG(LOG_INFO, frameLen == outputRingBuf.getFrame(sspbuf, frameLen),
+                       "Faile to get frame from outputRingBuf");
+            sf_writef_short(outwav, sspbuf, frameLen);
+        }
     }
 
-    ret = dios_ssp_uninit_api(hssp, &param);
-    if (OK_AUDIO_PROCESS != ret) {
-        fprintf(stderr, "dios_ssp_uninit_api failed\n");
-        return -1;
-    }
+    ASSERT_LOG_RET(OK_AUDIO_PROCESS == dios_ssp_uninit_api(hssp, &param), -1,
+                   "dios_ssp_uninit_api failed");
 
     /* Close input and output files. */
     sf_close(outwav);
     sf_close(inwav);
-    if (!refFilePath.empty()) {
-        sf_close(refwav);
-    }
+    if (!refFilePath.empty()) { sf_close(refwav); }
 
     return 0;
 }
