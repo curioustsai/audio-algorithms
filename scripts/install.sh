8369aafc5f71c70a2dc3d06afc1118b259f0c0ed
ui-richard <richard.tsai@ui.com>
2022-01-20 15:26:21 +0800

    remove(scripts): out of use folder, and update .gitignore

--- scripts/install.sh
+++
@@ -1,8 +0,0 @@
-PWD=`pwd`
-
-for p in gen3s gen3m gen3l gen4l;
-do
-	cp $PWD/build/$p/MinSizeRel/rootfs/include/audio_events/* $mw/sources/ubnt_smart_audio/include/ubnt_smart_audio/
-	[ ! -z $mw/sources/ubnt_smart_audio/lib/$p/ ] &&  mkdir -p $mw/sources/ubnt_smart_audio/lib/$p/
-	cp $PWD/build/$p/MinSizeRel/rootfs/lib/libaudio_events.a $mw/sources/ubnt_smart_audio/lib/$p/libaudio_events.a
-done
