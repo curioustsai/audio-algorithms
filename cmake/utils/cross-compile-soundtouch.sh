7675b3cad797f0f4b0e6976724d370462a517dd2
Jimmy Liao <jimmy.liao@ui.com>
2022-03-24 11:40:22 +0800

    set download folder as .ubnt_audio

--- cmake/utils/cross-compile-soundtouch.sh
+++ cmake/utils/cross-compile-soundtouch.sh
@@ -42,42 +42,42 @@ make clean -j 8
 set -x
 
 if [ "$platform" == "gen4l" ]; then
-	export PATH="$PATH:${HOME}/.ubnt/cache/gen4l/toolchains/bin/"
+	export PATH="$PATH:${HOME}/.ubnt_audio/cache/gen4l/toolchains/bin/"
 	export CPPFLAGS="-pipe -march=armv8-a -mcpu=cortex-a53+crypto -mlittle-endian -funwind-tables"
 	./configure --host=aarch64-linux-gnu --build=x86_64-pc-linux-gnu \
                 --enable-integer-samples \
 				--enable-shared=no --prefix=${project_root}/build/${platform}/
 
 elif [ "$platform" == "gen4c" ]; then
-	export PATH="$PATH:${HOME}/.ubnt/cache/gen4c/toolchains/bin/"
+	export PATH="$PATH:${HOME}/.ubnt_audio/cache/gen4c/toolchains/bin/"
 	export CPPFLAGS="-pipe -march=armv8-a -mcpu=cortex-a53+crypto -mlittle-endian -funwind-tables"
 	./configure --host=aarch64-linux-gnu --build=x86_64-pc-linux-gnu \
                 --enable-integer-samples \
 				--enable-shared=no --prefix=${project_root}/build/${platform}/
 
 elif [ "$platform" == "gen4s" ]; then
-	export PATH="$PATH:${HOME}/.ubnt/cache/gen4s/toolchains/bin/"
+	export PATH="$PATH:${HOME}/.ubnt_audio/cache/gen4s/toolchains/bin/"
 	export CPPFLAGS="-mthumb -march=armv7-a -mtune=cortex-a7 -mlittle-endian -mfloat-abi=hard -mfpu=neon-vfpv4 -Wno-deprecated-declarations -Wa,-mimplicit-it=thumb -Wno-psabi"
 	./configure --host=arm-linux-gnueabihf --build=x86_64-pc-linux-gnu \
                 --enable-integer-samples \
 				--enable-shared=no --prefix=${project_root}/build/${platform}/
 
 elif [ "$platform" == "gen3l" ]; then
-	export PATH="$PATH:${HOME}/.ubnt/cache/gen3l/toolchains/bin/"
+	export PATH="$PATH:${HOME}/.ubnt_audio/cache/gen3l/toolchains/bin/"
 	export CPPFLAGS='-mthumb -march=armv7-a -mtune=cortex-a9 -mlittle-endian -mfloat-abi=hard -mfpu=neon -Wno-deprecated-declarations -Wa,-mimplicit-it=thumb -funwind-tables'
 	./configure --host=arm-openwrt-linux --build=x86_64-pc-linux-gnu \
 				--with-fft=gpl-fftw3 --enable-fixed-point \
 				--enable-shared=no --prefix="${project_root}/build/${platform}"
 
 elif [ "$platform" == "gen3m" ]; then
-	export PATH="$PATH:${HOME}/.ubnt/cache/gen3m/toolchains/bin/"
+	export PATH="$PATH:${HOME}/.ubnt_audio/cache/gen3m/toolchains/bin/"
 	export CPPFLAGS='-mthumb -march=armv7-a -mtune=cortex-a9 -mlittle-endian -mfloat-abi=hard -mfpu=neon -Wno-deprecated-declarations -Wa,-mimplicit-it=thumb -funwind-tables'
 	./configure --host=arm-openwrt-linux --build=x86_64-pc-linux-gnu \
 				--with-fft=gpl-fftw3 --enable-fixed-point \
 				--enable-shared=no --prefix="${project_root}/build/${platform}"
 
 elif [ "$platform" == "gen3s" ]; then
-	export PATH="$PATH:${HOME}/.ubnt/cache/gen3s/toolchains/bin/"
+	export PATH="$PATH:${HOME}/.ubnt_audio/cache/gen3s/toolchains/bin/"
 	export CPPFLAGS="-mthumb -march=armv7-a -mtune=cortex-a7 -mlittle-endian -mfloat-abi=hard -mfpu=neon-vfpv4 -Wno-deprecated-declarations -Wa,-mimplicit-it=thumb -funwind-tables -Wno-psabi"
 	./configure --host=arm-linux-gnueabihf --build=x86_64-pc-linux-gnu \
                 --enable-integer-samples \
